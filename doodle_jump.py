8d1221e7f08627675e13aaada8d002a98a6b1d52
Paavan Trivedi <2021002171210190@ljku.edu.in>
2023-08-30 20:25:16 +0530

    Update doodle_jump.py

--- doodle_jump.py
+++ doodle_jump.py
@@ -2,14 +2,14 @@ import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
 import random
 import math
 
-CANVAS_WIDTH = 400
-CANVAS_HEIGHT = 600
-PLATFORM_WIDTH = 80
-PLATFORM_HEIGHT = 10
+WIDTH = 400
+HEIGHT = 600
+STAGE_WIDTH = 80
+STAGE_HEIGHT = 10
 JUMP_VELOCITY = -10
 GRAVITY = 0.5
 
-doodle_pos = [CANVAS_WIDTH / 2, CANVAS_HEIGHT - 50]
+doodle_pos = [WIDTH / 2, HEIGHT - 50]
 doodle_vel = [0, JUMP_VELOCITY]
 platforms = [[100, 500]]
 doodle_image = simplegui.load_image('doodle.png')
@@ -30,27 +30,27 @@ def draw(canvas):
 
     for platform in platforms:
         canvas.draw_polygon([(platform[0], platform[1]),
-                             (platform[0] + PLATFORM_WIDTH, platform[1]),
-                             (platform[0] + PLATFORM_WIDTH,
-                              platform[1] + PLATFORM_HEIGHT),
-                             (platform[0], platform[1] + PLATFORM_HEIGHT)], 1, 'black', 'black')
+                             (platform[0] + STAGE_WIDTH, platform[1]),
+                             (platform[0] + STAGE_WIDTH,
+                              platform[1] + STAGE_HEIGHT),
+                             (platform[0], platform[1] + STAGE_HEIGHT)], 1, 'black', 'black')
 
     for platform in platforms:
-        if doodle_pos[1] + 20 > platform[1] and doodle_pos[1] + 20 < platform[1] + PLATFORM_HEIGHT:
-            if doodle_pos[0] > platform[0] and doodle_pos[0] < platform[0] + PLATFORM_WIDTH:
+        if doodle_pos[1] + 20 > platform[1] and doodle_pos[1] + 20 < platform[1] + STAGE_HEIGHT:
+            if doodle_pos[0] > platform[0] and doodle_pos[0] < platform[0] + STAGE_WIDTH:
                 doodle_vel[1] = JUMP_VELOCITY
                 score += 1
 
     platforms = [platform for platform in platforms if platform[1]
-                 > -PLATFORM_HEIGHT]
+                 > -STAGE_HEIGHT]
 
     if random.random() < 0.02:
-        x_pos = random.randint(0, CANVAS_WIDTH - PLATFORM_WIDTH)
+        x_pos = random.randint(0, WIDTH - STAGE_WIDTH)
         y_pos = platforms[-1][1] - random.randint(50, 200)
         platforms.append([x_pos, y_pos])
 
 
-Frame = simplegui.create_frame("Doodle Jump", CANVAS_WIDTH, CANVAS_HEIGHT)
+Frame = simplegui.create_frame("Doodle Jump", WIDTH, HEIGHT)
 Frame.set_draw_handler(draw)
 
 Frame.start()
